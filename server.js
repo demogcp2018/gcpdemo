d1e5540d8837aadbce87fcebaf48fcf7f977ea9b
Mahesh Kumar <maheshkumarrp@gmail.com>
2018-09-16 08:48:06 +0530

    See the magic

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello World from Edureka Team to Google Next Event");
+  response.end("Hello World with Cloud Build Magic");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
