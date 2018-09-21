e3cafda82f20abe5d72635a6994f60808c320f30
Mahesh Kumar <maheshkumarrp@gmail.com>
2018-09-21 15:46:21 +0530

    Magic works

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello World with Cloud Build Magic!!!!");
+  response.end("Hello World with Cloud Build Magic!!!!!");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
