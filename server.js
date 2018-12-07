ebf624e21854b1ff8c2e665bbedd86fed27f0b18
demogcp2018 <41831007+demogcp2018@users.noreply.github.com>
2018-12-08 05:26:23 +0530

    Updated the message

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello World to deployed using Cloud Build");
+  response.end("Hello World");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
